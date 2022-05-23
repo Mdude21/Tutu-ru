f48e16b654d1c34cd975d3b60d16c9fc9cb0c8b5
Mdude21 <kamal.valenok@gmail.com>
2022-05-23 12:42:06 +0300

    add gradle, manifest

---
+++ app/src/main/java/ru/mdude21/tutu/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.mdude21.tutu.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.mdude21.tutu.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
